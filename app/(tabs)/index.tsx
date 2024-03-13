049ce1bbf5454f213c2b807050199f7b7060b389
src19 <sr3243@srmist.edu.in>
2024-03-13 23:25:05 +0530

    added static event details for ui

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,39 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native'; 
+import events from '@/assets/data/events';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
-
+const event = events[0];
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab 1</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Image source={{uri:event.image}} style={styles.image}/>
+      <Text style={styles.title}>{event.clubname}</Text>
+      <Text style={styles.venue}>{event.venue}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 10,
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  venue : {
+    fontSize: 15,
+    fontWeight: '500',
+    marginVertical: 10,
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
   },
+  image: {
+    width: '100%',
+    aspectRatio: 1
+  }
 });
---
+++ assets/data/events.ts
@@ -0,0 +1,22 @@
+const events = [
+    {
+        id: 1,
+        clubname: 'SRMKzilla',
+        image: 'https://i.pinimg.com/736x/95/04/ea/9504ea9721e01951bb73ddb781399abc.jpg',
+        venue: 'Hippocrys Hall'
+    },
+    {
+        id: 2,
+        clubname: 'SRM Hackathon',
+        image: 'https://i.pinimg.com/originals/55/36/35/55363525f4601cd6a9eff0884ff59845.jpg',
+        venue: 'TP Ganeshan Audi'
+    },
+    {
+        id: 3,
+        clubname: 'Gen-Y',
+        image: 'https://media.istockphoto.com/id/1189767041/vector/hackathon-signs-round-design-template-thin-line-icon-concept-vector.jpg?s=612x612&w=0&k=20&c=DW-btIjpNjItFfk35N4KvrMkoGoqd1rEPwb_uV9IZEU=',
+        venue: 'TP 403'
+    },
+];
+
+export default events;
